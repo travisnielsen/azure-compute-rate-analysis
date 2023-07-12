5bef718c71e659ae06a83657b10fb8c730efbf7d
Travis Nielsen <trniel@microsoft.com>
2023-07-11 19:10:11 -0500

    updates

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is a set of scripts to assist with analysis of Azure API call rates and thr
 
 * Recent version of [PowerShell 7](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.3)
 * Recent version of the [Azure PowerShell Module](https://learn.microsoft.com/en-us/powershell/azure/install-azure-powershell?view=azps-10.1.0)
-* A blob storage account with a Shared Access Signature for create / write operations
+* A blob storage account with a Shared Access Signature for create / write operations (set at the container level)
 
 ## Configuration
 
@@ -20,4 +20,4 @@ Create a file name `config.json` in the `/scripts` directory and add the followi
 }
 ```
 
-Open a PowerShell terminal, switch to the `scripts` directory, and run `export-stats.ps1`. You may also want to run individual commands step-by-step.
+Open a PowerShell terminal, switch to the `scripts` directory, and run `export-stats.ps1`. You may need to run individual commands step-by-step. Output files will be written to the container your specified in the SAS URI. You may use Azure Storage Explorer or the Storage Browser in the Azure Portal to access the files.
--- scripts/export-stats.ps1
+++ scripts/export-stats.ps1
@@ -5,6 +5,14 @@ $sasUri = $contextInfo.sasUri
 $fromTime = (Get-Date).AddDays(-3)
 $toTime = Get-Date
 
+# For now, run each command individually and download the file once its writtn to the storage account. This command does not support changing the blob name when it write the file.
 Export-AzLogAnalyticRequestRateByInterval -Location "East US" -FromTime $fromTime -ToTime $toTime -IntervalLength ThirtyMins -BlobContainerSasUri $sasUri -GroupByApplicationId
-Export-AzLogAnalyticRequestRateByInterval -Location "East US" -FromTime $fromTime -ToTime $toTime -IntervalLength ThirtyMins -BlobContainerSasUri $sasUri -GroupByApplicationId
+Export-AzLogAnalyticRequestRateByInterval -Location "East US" -FromTime $fromTime -ToTime $toTime -IntervalLength ThirtyMins -BlobContainerSasUri $sasUri -GroupByOperationName
+Export-AzLogAnalyticRequestRateByInterval -Location "East US" -FromTime $fromTime -ToTime $toTime -IntervalLength ThirtyMins -BlobContainerSasUri $sasUri -GroupByResourceName
+Export-AzLogAnalyticRequestRateByInterval -Location "East US" -FromTime $fromTime -ToTime $toTime -IntervalLength ThirtyMins -BlobContainerSasUri $sasUri -GroupByThrottlePolicy
 
+Export-AzLogAnalyticThrottledRequest -Location "East US" -FromTime $fromTime -ToTime $toTime -BlobContainerSasUri $sasUri -GroupByApplicationId
+Export-AzLogAnalyticThrottledRequest -Location "East US" -FromTime $fromTime -ToTime $toTime -BlobContainerSasUri $sasUri -GroupByOperationName
+Export-AzLogAnalyticThrottledRequest -Location "East US" -FromTime $fromTime -ToTime $toTime -BlobContainerSasUri $sasUri -GroupByResourceName
+Export-AzLogAnalyticThrottledRequest -Location "East US" -FromTime $fromTime -ToTime $toTime -BlobContainerSasUri $sasUri -GroupByThrottlePolicy
+Export-AzLogAnalyticThrottledRequest -Location "East US" -FromTime $fromTime -ToTime $toTime -BlobContainerSasUri $sasUri -GroupByApplicationId
